e38ab016f6c400611adf28bf2903279d2fc1f87e
soumya-ramteke <soumya.r@cgi.com>
2018-09-17 10:15:46 +0530

    without StringBuilder, added comments

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,21 +7,21 @@ namespace CaesarCipher
     public static class RotationalCipher
     {
         public static string Rotate(string text, int shiftKey)
-        {
+        {   //initialize empty string
             string cipher = string.Empty;
+            //loop through each char
             foreach(char ch in text)
-            {
+            {  //find the cipher char
                cipher+=CipherHelper(ch,shiftKey);
             }
             return cipher;
         }
         
-        public static char CipherHelper(char ch, int shiftKey) {  
-            if (!char.IsLetter(ch)) {  
-                return ch;  
-            }  
-  
-            char d = char.IsUpper(ch) ? 'A' : 'a';  
+        public static char CipherHelper(char ch, int shiftKey) 
+        {   
+            char d = char.IsUpper(ch) ? 'A' : 'a'; 
+            // simply returns the char after adding the shiftKey
+            //if it exceeds Z or z then we need %26
             return (char)((((ch + shiftKey) - d) % 26) + d);  
         }  
 
